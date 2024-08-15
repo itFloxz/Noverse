a2df73f81cd47e6a63ed556c2bdd37aecf0f4feb
itFloxz <nightzaa103@gmail.com>
2024-08-15 14:56:24 +0700

    Complete Part5
    Complete Part5

--- Music-project/frontend/src/components/Signup.jsx
+++ Music-project/frontend/src/components/Signup.jsx
@@ -1,35 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
+import { toast } from "react-toastify"
 
 const Signup = () => {
+  const navigate = useNavigate()
+  const [formdata,setFormData]=useState({
+    email:"",
+    first_name:"",
+    last_name:"",
+    password:"",
+    password2:""
+  })
+
+  const [error, setError]=useState("")
+
+  const handleOnChange = (e)=>{
+    setFormData({...formdata,[e.target.name]:e.target.value})
+  }
+const {email, first_name, last_name, password, password2}=formdata
+  
+const handleSubmit = async (e)=>{
+    e.preventDefault()
+    if(!email || !first_name || !last_name || !password || !password2){
+      setError("all fields are required")
+    }else {
+      console.log(formdata)
+      //make call to api
+      const res = await axios.post("http://localhost:8000/api/v1/auth/register/", formdata)
+      //check our response
+      const response = res.data
+      console.log(response)
+      if (res.status === 201){
+      //redurect to verifyemail component
+        navigate("/otp/verify")
+        toast.success(response.massage)
+
+      }
+      
+      //server error pass to error
+    }
+    
+  }
+
+
   return (
     <div>
       <div className="form-container">
         <div style={{ width: "100%" }} className="wrapper">
           <h2>Create Account</h2>
-          <form>
+          
+          <form onSubmit={handleSubmit}>
+          <p style={{color:"red", padding:"1px"}}>{error ? error : ""}</p>
             <div className="form-group">
               <label htmlFor="">Email Address:</label>
-              <input type="text" className="email-form" name="email" />
+              <input 
+              type="text" className="email-form" name="email" value={email} onChange={handleOnChange}/>
             </div>
             <div className="form-group">
               <label htmlFor="">First Name:</label>
-              <input type="text" className="email-form" name="First_name" />
+              <input type="text" className="email-form" name="first_name" value={first_name} onChange={handleOnChange}/>
             </div>
             <div className="form-group">
               <label htmlFor="">Last Name:</label>
-              <input type="text" className="email-form" name="Last_name" />
+              <input type="text" className="email-form" name="last_name" value={last_name } onChange={handleOnChange}/>
             </div>
             <div className="form-group">
               <label htmlFor="">Password:</label>
-              <input type="password" className="email-form" name="password" />
+              <input type="password" className="email-form" name="password" value={password} onChange={handleOnChange}/>
             </div>
             <div className="form-group">
               <label htmlFor="">Confirm Password:</label>
               <input
-                type="password"
-                className="email-form"
-                name="confirm_password"
-              />
+                type="password" className="email-form" name="password2" value={password2} onChange={handleOnChange}/>
             </div>
             <input type="submit" value="Submit" className="submitButton" />
           </form>
--- Music-project/frontend/src/components/VerifyEmail.jsx
+++ Music-project/frontend/src/components/VerifyEmail.jsx
@@ -1,13 +1,30 @@
+import axios from "axios";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"
+import { toast } from "react-toastify"
 
 const VerifyEmail = () => {
+  const [otp, setOtp]=useState("")
+  const navigate=useNavigate()
+
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+    if (otp) {
+      const response = await axios.post("http://localhost:8000/api/v1/auth/verify-email/", {'otp':otp})
+      if (response.status === 200) {
+        navigate("/login")
+        toast.success(response.data.massage)
+      }
+    }
+  }
   return (
     <div>
       <div className="form-container">
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="">Enter your OTP code:</label>
-            <input type="text" className="email-form" name="otp" />
+            <input type="text" className="email-form" name="otp" value={otp} onChange={(e)=>setOtp(e.target.value)}/>
             <div />
           </div>
           <input type="submit" className="vbtn" value="Send" />
