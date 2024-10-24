0fa904f974fc33075431e453e7dd521ca151734d
itFloxz <nightzaa103@gmail.com>
2024-10-24 12:57:41 +0700

    resetpassword
    ทำ Reset password ผ่านหน้า web และให้ไป update หลังบ้านได้แล้ววววววววววววววววววววววววววววววววววววววววววววววววววววววววววววววววววววว

--- Music-project/django_rest_auth/accounts/views.py
+++ Music-project/django_rest_auth/accounts/views.py
@@ -12,7 +12,11 @@ from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from rest_framework.permissions import AllowAny
 from django.conf import settings
 from django.core.mail import send_mail
+from django.contrib.auth.hashers import check_password
+
 # Create your views here.
+
+
 
 class RegisterUserView(GenericAPIView):
     serializer_class = UserRegisterSerializer
@@ -108,4 +112,20 @@ class LogoutUserView(GenericAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(status=status.HTTP_200_OK)
+
+class ChangePasswordView(GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        old_password = request.data.get('old_password')
+        new_password = request.data.get('new_password')
+
+        if not check_password(old_password, user.password):
+            return Response({'message': 'Old password is incorrect'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user.set_password(new_password)
+        user.save()
+        return Response({'message': 'Password changed successfully'}, status=status.HTTP_200_OK)
+    
     
--- Music-project/django_rest_auth/django_rest_auth/urls.py
+++ Music-project/django_rest_auth/django_rest_auth/urls.py
@@ -18,6 +18,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('accounts.urls')),  # Ensure trailing slash here
